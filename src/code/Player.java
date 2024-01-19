2a5091c5e0148acf9d6e87a891016fab34b0d5b4
Zebaka Yame <98798444+zebakayame@users.noreply.github.com>
2024-01-19 21:24:27 +0100

    Changed assets directory and made fancier

--- src/code/Player.java
+++ src/code/Player.java
@@ -24,6 +24,17 @@ public class Player {
 
     public String imgStat = "down";
 
+    // Get the current of the project
+    private String playerTexturePath = System.getProperty("user.dir") + "/assets/Texture/player.png";
+
+    // Image
+    BufferedImage img;
+    BufferedImage imgdown;
+    BufferedImage imgup;
+    BufferedImage imgleft;
+    BufferedImage imgright;
+    BufferedImage imgToDraw;
+
     public Player(KeyManager keyM, GamePanel gp){
         this.keyM = keyM; // settings up the keyManager so the player can access it
         this.gp = gp;
@@ -32,28 +43,14 @@ public class Player {
     public void updatePlayer(){
         positionUpdate();
 
-
+        //System.out.println(playerTexturePath);
         deb.consoleDebog("Player position: " + playerX + "\t" + playerY);
     }
 
     public void drawPlayer(Graphics2D g2){
         
-        BufferedImage img = null;
-        BufferedImage imgdown = null;
-        BufferedImage imgup = null;
-        BufferedImage imgleft = null;
-        BufferedImage imgright = null;
-        BufferedImage imgToDraw = null;
-        try {
-            img = ImageIO.read(new File("/home/kuku/Documents/VisualStudioWorkSpace/Project/Neko_Adventure/src/ressource/Texture/player.png"));
-            imgdown = img.getSubimage(0, 0 , 64, 64); // Gets the sub image of the player charachter
-            imgup = img.getSubimage(64, 0, 64, 64);
-            imgright = img.getSubimage(128, 0, 64, 64);
-            imgleft = img.getSubimage(192, 0, 64, 64);
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-        
+        // Call the function to fix player Texture
+        setPlayerTexture();
 
         // Movement CLASS
         // Depending on Stat of hte image (imgStat), show specific Image for movement
@@ -85,6 +82,19 @@ public class Player {
                 break;
         }
 
+    }
+
+    // Call the function to fix player Texture
+    private void setPlayerTexture() {
+        try {
+            img = ImageIO.read(new File(playerTexturePath));
+            imgdown = img.getSubimage(0, 0 , 64, 64); // Gets the sub image of the player charachter
+            imgup = img.getSubimage(64, 0, 64, 64);
+            imgright = img.getSubimage(128, 0, 64, 64);
+            imgleft = img.getSubimage(192, 0, 64, 64);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     private void positionUpdate() {
